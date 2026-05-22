ec68c7be38a44aa6f508bdc9b6b90f00f2ccccba
devjoaol <124259261+devjoaol@users.noreply.github.com>
2026-05-22 14:25:10 -0300

    feat: adiciona suporte a criptografia de senhas com bcrypt e validações de entrada no DTO de usuário

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,13 +1,17 @@
-import { IsEmail, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsString, MinLength, IsNotEmpty } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class CreateUserDto {
-    @IsString()
-    @MinLength(2)
-    nome!: string;
+  @IsString()
+  @MinLength(2)
+  @IsNotEmpty({ message: 'Nome não pode ser vazio' })
+  nome!: string;
 
-    @IsEmail()
-    email!: string;
-    
-    @MinLength(6)
-    senha!: string;
+  @IsEmail()
+  @Transform(({ value }) => value.toLowerCase())
+  @IsNotEmpty({ message: 'Email não pode ser vazio' })
+  email!: string;
+
+  @MinLength(6)
+  senha!: string;
 }
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,41 +1,47 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { DrizzleService } from '../db/drizzle.service'
+import { DrizzleService } from '../db/drizzle.service';
 import { usuarios } from 'src/db/schema';
 import { eq } from 'drizzle-orm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(private db: DrizzleService) {}
-  
+
   async create(dto: CreateUserDto) {
+    const hash = await bcrypt.hash(dto.senha, 10);
     const user = await this.db.db
-    .insert(usuarios)
-    .values(dto)
-    .returning();
-    return user[0]
+      .insert(usuarios)
+      .values({
+        nome: dto.nome,
+        email: dto.email,
+        senhaHash: hash,
+      })
+      .returning();
+    return user[0];
   }
 
   async findAll() {
     return await this.db.db.select().from(usuarios);
   }
 
-  async findOne(id: number) {
+  async findOne(id: string) {
     const user = await this.db.db
       .select()
       .from(usuarios)
       .where(eq(usuarios.id, id))
       .limit(1);
 
-      if(!user.length) {
-        throw new NotFoundException('User not found');
-      }
+    if (!user.length) {
+      throw new NotFoundException('User not found');
+    }
 
-      return user[0];
+    return user[0];
   }
 
-  async update(id: number, dto: UpdateUserDto) {
+  async update(id: string, dto: UpdateUserDto) {
     const user = await this.db.db
       .update(usuarios)
       .set({
@@ -52,7 +58,7 @@ export class UsersService {
     return user[0];
   }
 
-  async remove(id: number) {
+  async remove(id: string) {
     const user = await this.db.db
       .delete(usuarios)
       .where(eq(usuarios.id, id))
@@ -65,6 +71,6 @@ export class UsersService {
     return {
       message: 'User deleted successfully',
       user: user[0],
+    };
   }
-}
 }
