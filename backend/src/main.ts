92b3a7af3ae18f85840f800d4fe262d08c47a6b2
Guilherme Marques <mr.gmrsouza@gmail.com>
2026-06-07 02:18:07 -0300

    feat: implement global exception filter and error response standard

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import cookieParser from "cookie-parser";
+import { GlobalExceptionFilter } from "./common/filters/exception.filter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,8 @@ async function bootstrap() {
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  app.useGlobalFilters(new GlobalExceptionFilter());
 
   await app.listen(process.env.PORT ?? 3300);
 }
