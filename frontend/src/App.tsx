b21ba324a40ac94cd014a89391017316b40b6acf
pauloisaque1543 <pauloisaquecontatocomercial@gmail.com>
2026-06-20 19:42:15 -0300

    feat(frontend):teste pra aprender

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <>
-    <h1 className="text-9xl text-red-500 font-bold">Hello World, Nego Bahia!</h1>
+      <h1 className="text-9xl text-green-500 font-bold">Hello World, Nego Moradia</h1>
     </>
   )
 }
