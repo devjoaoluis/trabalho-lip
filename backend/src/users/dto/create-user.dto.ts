3d761fe3b13b563b64b2d8905ca646a7221b0c29
devjoaol <124259261+devjoaol@users.noreply.github.com>
2026-06-21 13:56:02 -0300

    feat: enhance user DTOs with Swagger decorators for improved API documentation and add user response structure

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -3,16 +3,30 @@ import { Transform } from "class-transformer";
 import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateUserDto {
+  @ApiProperty({
+    example: "João Luis Gomes",
+    description: "Nome completo do usuário",
+    minLength: 2,
+  })
   @IsString()
   @MinLength(2)
   @IsNotEmpty({ message: "Nome não pode ser vazio" })
   nome!: string;
 
+  @ApiProperty({
+    example: "joaoluis@gmail.com",
+    description: "E-mail do usuário",
+  })
   @IsEmail()
   @Transform(({ value }) => value.toLowerCase())
   @IsNotEmpty({ message: "Email não pode ser vazio" })
   email!: string;
 
+  @ApiProperty({
+    example: "senha123",
+    description: "Senha do usuário com no mínimo 6 caracteres.",
+    minLength: 6,
+  })
   @IsString()
   @MinLength(6)
   senha!: string;
