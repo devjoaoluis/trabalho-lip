c2cfdaa2c3bf4a18c08a6d741480092320f561a4
Guilherme Marques <mr.gmrsouza@gmail.com>
2026-06-21 17:41:56 -0300

    feat: implementar logger middleware e integrar middlewares no bootstrap

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,10 +3,17 @@ import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import cookieParser from "cookie-parser";
 import { GlobalExceptionFilter } from "./common/filters/exception.filter";
+import helmet from "helmet";
+import morgan from "morgan";
+import logger from "./config/logger";
+import express from "express";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.use(helmet());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(morgan("combined", { stream: { write: message => logger.info(message.trim()) } }));
 
   app.enableCors({
     origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:3000",
